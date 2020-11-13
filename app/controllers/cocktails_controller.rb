2449b0b914f115e10d5ae7b61b666399d0dc1bad
Arthur Amon <aamontoo@gmail.com>
2020-11-13 17:57:57 +0100

    Solve creating new cocktail bug'

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -20,7 +20,7 @@ class CocktailsController < ApplicationController
     if @cocktail.save
       redirect_to cocktail_path(@cocktail)
     else
-      render :new
+      render 'cocktails/index'
     end
   end
 
